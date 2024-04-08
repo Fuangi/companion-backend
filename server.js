7796b1a9f5ffdcd64ead6a1cf0bf80c2466f2faa
Vanessa Fuangi <vanessafuangi@gmail.com>
2024-04-08 14:41:51 +0100

    Linked the application to the backend

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+// Connecting to the database
+const DB = "mongodb://127.0.0.1:27017/companion";
+
+mongoose.connect(DB).then((con) => {
+  console.log("Database connection successful");
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("App running on port " + port);
+});
+
+// handling unhandled rejected promises - from asynchronous code
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 🔴 Shutting down...");
+
+  console.log(err.name, err.message);
+  // server.close(() => {
+  process.exit(1);
+  // });
 });
