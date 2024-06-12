cac63d89fb54fe44db2ca9614becd414f0b565d1
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-06-12 18:14:27 +0100

    Changed the group controller for get all groups

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,9 +1,36 @@
 const Group = require("../models/groupModel");
 const factory = require("./handlerFactory");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.createGroup = factory.createOne(Group);
 
-exports.getAllGroups = factory.getAll(Group);
+// exports.getAllGroups = factory.getAll(Group);
+exports.getAllGroups = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(
+    // Model.find({ userId: req.user._id }),
+    Group.find(),
+    req.query
+  )
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const docs = await features.query;
+  // const docs = await features.query.explain();
+  // docs.sort().select().skip().limit();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: "success",
+    results: docs.length,
+    data: {
+      data: docs,
+    },
+  });
+});
 
 exports.getGroup = factory.getOne(Group, { path: "members", select: "name" });
 
